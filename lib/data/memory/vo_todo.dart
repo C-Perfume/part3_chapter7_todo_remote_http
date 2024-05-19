123fa3b7ac3677bc8d0dc84db2247cefd8b893d2
C-Perfume <akdl1020@gmail.com>
2024-05-19 19:26:13 +0900

    예제 따라하기
    네트워크 최적화로 클라이언트 다트 파일 내용 가져오려 브랜치 변경 시 머지 해야 다시 예제용 브랜치로 돌아갈 수 있는 안내 창 확인함

--- lib/data/memory/vo_todo.dart
+++ lib/data/memory/vo_todo.dart
@@ -1,55 +1,18 @@
 import 'package:fast_app_base/data/local/collection/todo_db_model.dart';
 import 'package:fast_app_base/data/memory/todo_status.dart';
-import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-class Todo {
-  final int id;
-  final DateTime createdTime;
-  DateTime? modifyTime;
-  String title;
-  DateTime dueDate;
-  TodoStatus status;
+part 'vo_todo.freezed.dart';
+part 'vo_todo.g.dart';
 
-<<<<<<< Updated upstream
-  Todo({
-    required this.id,
-    required this.createdTime,
-    this.modifyTime,
-    required this.title,
-    required this.dueDate,
-    this.status = TodoStatus.incomplete,
-  });
-=======
 @unfreezed
 class Todo with _$Todo{
->>>>>>> Stashed changes
 
-  factory Todo.fromJson(Map<String, Object?> json) {
-    return Todo(
-      id: json['id'] as int,
-      createdTime: DateTime.parse(json['createdTime'] as String),
-      modifyTime: json['modifyTime'] == null ? null : DateTime.parse(json['modifyTime'] as String),
-      title: json['title'] as String,
-      dueDate: DateTime.parse(json['dueDate'] as String),
-      status: TodoStatus.values.asNameMap()[json['status']] ?? TodoStatus.unknown,
-    );
-  }
+  Todo._();
 
-<<<<<<< Updated upstream
-  Map<String, dynamic> toJson() {
-    return <String, dynamic>{
-      'id': id,
-      'createdTime': createdTime.toIso8601String(),
-      'modifyTime': modifyTime?.toIso8601String(),
-      'title': title,
-      'dueDate': dueDate.toIso8601String(),
-      'status': describeEnum(status),
-    };
-  }
-=======
   factory Todo({
     required final int id,
-    @JsonKey(name: 'created_time')required final DateTime createdTime,
+    required final DateTime createdTime,
     DateTime? modifyTime,
     required String title,
     required DateTime dueDate,
@@ -57,7 +20,6 @@ class Todo with _$Todo{
   }) = _Todo;
 
   factory Todo.fromJson(Map<String, Object?> json) => _$TodoFromJson(json);
->>>>>>> Stashed changes
 
   TodoDbModel get dbModel => TodoDbModel(id, createdTime, modifyTime, title, dueDate, status);
 }
